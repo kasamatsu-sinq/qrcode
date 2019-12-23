1c8afa85391e866ed9804fa7027b3e6b3272f3a7
Kasamatsu <kasamatsu@sinq.co.jp>
2019-12-23 17:00:35 +0900

    修正

--- js/app.js
+++ js/app.js
@@ -1,50 +1,54 @@
-if (!navigator.mediaDevices) {
-    document.querySelector('#js-unsupported').classList.add('is-show')
-}
 
-const video  = document.querySelector('#js-video')
+
+const video = document.querySelector('#js-video')
 const canvas = document.querySelector('#js-canvas')
-const ctx    = canvas.getContext('2d')
+const ctx = canvas.getContext('2d')
 
 const checkImage = () => {
-    // 取得している動画をCanvasに描画
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+	// 取得している動画をCanvasに描画
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
 
-    // Canvasからデータを取得
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+	// Canvasからデータを取得
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-    // jsQRに渡す
-    const code = jsQR(imageData.data, canvas.width, canvas.height)
+	// jsQRに渡す
+	const code = jsQR(imageData.data, canvas.width, canvas.height)
 
-    // QRコードの読み取りに成功したらモーダル開く
-    // 失敗したら再度実行
-    if (code) {
-        openModal(code.data)
-    } else {
-        setTimeout(() => { checkImage() }, 200)
-    }
+	// QRコードの読み取りに成功したらモーダル開く
+	// 失敗したら再度実行
+	if (code) {
+		openModal(code.data)
+	} else {
+		setTimeout(() => { checkImage() }, 200)
+	}
 }
 
-const openModal = function(url) {
+const openModal = function (url) {
 	document.querySelector(".result_wrapper").html(url);
-    // document.querySelector('#js-result').innerText = url
-    // document.querySelector('#js-link').setAttribute('href', url)
-    // document.querySelector('#js-modal').classList.add('is-show')
+	// document.querySelector('#js-result').innerText = url
+	// document.querySelector('#js-link').setAttribute('href', url)
+	// document.querySelector('#js-modal').classList.add('is-show')
 }
 
-navigator.mediaDevices
-    .getUserMedia({
-        audio: false,
-        video: {
-            facingMode: {
-                exact: 'environment'
-            }
-        }
-    })
-    .then(function(stream) {
-        video.srcObject = stream
-        video.onloadedmetadata = function(e) {
-            video.play()
-            checkImage()
-        }
-    })
+if (!navigator.mediaDevices) {
+	document.querySelector('#js-unsupported').classList.add('is-show')
+} else {
+	navigator.mediaDevices
+		.getUserMedia({
+			audio: false,
+			video: {
+				facingMode: {
+					exact: 'environment'
+				}
+			}
+		})
+		.then(function (stream) {
+			video.srcObject = stream
+			video.onloadedmetadata = function (e) {
+				video.play()
+				checkImage()
+			}
+		})
+
+}
+
